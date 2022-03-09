fdf231519643a0c3e67905168d7d45bb7325741f
diomarcosta <54088074+diomarcosta@users.noreply.github.com>
2022-03-08 22:43:33 -0300

    Update aut_whatsapp.py

--- aut_whatsapp.py
+++ aut_whatsapp.py
@@ -1,16 +1,30 @@
 import PySimpleGUI as sg
 import webbrowser as wb
 
-#define the layout
-layout = [[sg.Text('Iniciar conversa whatsapp')],
-            [sg.Text('Digite o número sem o código do país, apenas com o DDD'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')]]
+sg.theme('DarkGray')
+# define the layout
+
+layout = [[sg.Text('Start WhastApp chat')],
+          [sg.Text(
+              'Country code', size=14), sg.InputText(size=5, key='-cod-')],
+          [sg.Text(
+              'DDD',  size=14), sg.InputText(size=5, key='-ddd-')],
+          [sg.Text(
+              'Number'), sg.InputText(size=15, key='-num-')],
+          [sg.Button('Open chat'), sg.Button('Cancel')]]
 
 # Create the Window
 window = sg.Window('Window Title', layout)
-#read the event
-event, values = window.read()
-#close the window
-window.close()
 
-wb.open("https://wa.me/55" + str(values[0]))
+while True:
+    # read the event
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+        break
+    elif values['-cod-'] == '' or values['-ddd-'] == '' or values['-num-'] == '':
+        sg.popup('Please fill all the fields!')
+    else:
+        wb.open("https://wa.me/" +
+                values['-cod-'] + values['-ddd-'] + values['-num-'])
+# close the window
+window.close()
